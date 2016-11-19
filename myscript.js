48b2bc1d22c9a2391cf44ce6fbfed86033e72319
kswcarey <kswcarey@gmail.com>
2016-11-19 14:41:27 -0500

    filter bar placement and styling improved
    Filter bar elements placement and styling cleaned up. Added rudimentary
    cards to start testing filter functions. No js yet.

---
+++ myscript.js
@@ -0,0 +1,12 @@
+//filtering results
+    //if user chooses filter, loop thru all cards
+    //keep card if it has a value that matches the filter/s
+    //remove card if none of filters match its value
+
+//resetting results
+
+//animating card out
+
+//animating cards back in
+
+//adding banner to return to site above any linked external page
